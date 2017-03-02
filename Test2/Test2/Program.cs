949bf9abc886f9ade827d77790a63765e0b9ba3c
PiotrStachaczynski <stachaczynski.piotr@gmail.com>
2017-03-02 23:44:32 +0100

    .NET
    Pętle i instr warunkowa

---
+++ Test2/Test2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=0;
+            for (int i = 0; i < 300; i++)
+            {
+                n++;
+                for (int j=0; j<i; i++)
+                {
+                    n++;
+                    if (n > 20)
+                    {
+                        Console.WriteLine(n);
+                    } 
+                }
+            }
+        }
+    }
+}
