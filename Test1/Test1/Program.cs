7dab80ca6f518cee2bf379f276e2f7584b5aae1b
PiotrStachaczynski <stachaczynski.piotr@gmail.com>
2017-03-02 19:18:22 +0100

    Nauka .NET
    Pierwszy program test składni i pisanie funkcji

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FeetToInches(30));
+            Console.WriteLine(FeetToInches(120));
+        }
+        static int FeetToInches(int feet)
+        {
+            int inches = feet * 12;
+            return inches;
+        }
+    }
+}
